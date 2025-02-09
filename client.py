7bd295cf28462659c53faea22e4e5b3051a09526
mohammed-saleh2007 <mohammed.saleh.777@proton.me>
2025-02-09 19:50:48 +0200

    update

--- client.py
+++ client.py
@@ -8,17 +8,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     while True:
         user_input = input("\033[31;1;4m[ * ] What to send?: \033[0m")
         msg = ""
-        for i in msg:
-            shifted = ord(i) + 3
-            msg += chr(shifted)
-        msg.encode()
         print(f"[ ! ] Connected to: {HOST}:{PORT}")
         s.sendall(msg)
-        print("[ + ] Sending...: {msg}")
+        print("[ + ] Sending...")
         print("[ * ] Receiving data ...")
         data = s.recv(1024 * 1024 * 1024)
-        for i in file_content:
-            shifted = ord(i) - 3
-            msg += chr(shifted)
-        print("[ + ] Receiving")
+        print("[ + ] Receiving...")
         print(f"\033[31;1;4m[ - ] Recieved: {msg!r}\033[0m")
--- server.py
+++ server.py
@@ -12,22 +12,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(f"[ ! ] Connected by {addr}")
         while True: 
             print("[ + ] Receiving data ...")
-            data = conn.recv(1024 * 1024 * 1024)
-            for i in data:
-                shifted = ord(i) - 3
-                msg += chr(shifted)
+            msg = conn.recv(1024 * 1024 * 1024)
             print(f"\033[31;1;4m[ - ] Recieved: {msg}\033[0m")
 
-            if b'sudo_end' == data:
+            if data == b'sudo_end':
                 print("[ ! ] No Data received")
                 print("[ - ] Closeing Connection")
                 break
 
             msg = input("\033[31;1;4m[ * ] What to send?: \033[0m")
-            for i in msg:
-                shifted = ord(i) + 3
-                msg += chr(shifted)
-            msg.encode()
             print(f"[ + ] Sending: {msg}")
             conn.sendall(msg)
 
